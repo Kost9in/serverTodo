4ee97a4ad5485b34d8af497ffed3302894b00824
Kost9In <kost9inbi4@mail.ru>
2016-08-03 12:05:31 +0300

    add error handler

--- routes/index.js
+++ routes/index.js
@@ -7,13 +7,25 @@ export default router => {
   router.prefix(`${API_PREFIX}/items`);
 
   router.get('/', async (ctx) => {
-    ctx.body = await itemSchema.find();
+    try {
+      ctx.body = await itemSchema.find();
+    }
+    catch(err) {
+      ctx.status = err.status || 500; 
+      ctx.body = 'Internal server error';
+    }
   });
 
   router.get('/:id', async (ctx) => {
     ctx.body = '';
     if (mongoose.Types.ObjectId.isValid(ctx.params.id)) {
-      ctx.body = await itemSchema.findById(ctx.params.id);
+      try {
+        ctx.body = await itemSchema.findById(ctx.params.id);
+      }
+      catch(err) {
+        ctx.status = err.status || 500; 
+        ctx.body = 'Internal server error';
+      }
     }
     if (!ctx.body) {
       ctx.response.status = 404;
@@ -24,7 +36,13 @@ export default router => {
   router.post('/', async (ctx) => {
     const item = (ctx.request.body.title) ? ctx.request.body.title.trim() : '';
     if (item) {
-      ctx.body = await new itemSchema({ title: 'test', time: new Date() }).save();
+      try {
+        ctx.body = await new itemSchema({ title: 'test', time: new Date() }).save();
+      }
+      catch(err) {
+        ctx.status = err.status || 500; 
+        ctx.body = 'Internal server error';
+      }
     } else {
       ctx.response.status = 400;
       ctx.body = { error: 'Validation error' };
@@ -37,7 +55,13 @@ export default router => {
     if (ctx.request.body.title) {
       if (mongoose.Types.ObjectId.isValid(ctx.params.id)) {
         ctx.request.body.time = new Date();
-        ctx.body = await itemSchema.findByIdAndUpdate(ctx.params.id, ctx.request.body);
+        try {
+          ctx.body = await itemSchema.findByIdAndUpdate(ctx.params.id, ctx.request.body);
+        }
+        catch(err) {
+          ctx.status = err.status || 500; 
+          ctx.body = 'Internal server error';
+        }
       }
       if (!ctx.body) {
         ctx.response.status = 404;
@@ -52,7 +76,13 @@ export default router => {
   router.delete('/:id', async (ctx) => {
     ctx.body = '';
     if (mongoose.Types.ObjectId.isValid(ctx.params.id)) {
-      ctx.body = await itemSchema.findByIdAndRemove(ctx.params.id);
+      try {
+        ctx.body = await itemSchema.findByIdAndRemove(ctx.params.id);
+      }
+      catch(err) {
+        ctx.status = err.status || 500; 
+        ctx.body = 'Internal server error';
+      }
     }
     if (!ctx.body) {
       ctx.response.status = 404;
