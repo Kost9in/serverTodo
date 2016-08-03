4c2135542fb3ae97b43c0dd2dda8ab7d6c1cd8b3
Kost9In <kost9inbi4@mail.ru>
2016-08-03 11:54:15 +0300

    add all routes

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import itemSchema from '../models';
 import { API_PREFIX } from '../config';
 
@@ -7,6 +8,17 @@ export default router => {
 
   router.get('/', async (ctx) => {
     ctx.body = await itemSchema.find();
+  });
+
+  router.get('/:id', async (ctx) => {
+    ctx.body = '';
+    if (mongoose.Types.ObjectId.isValid(ctx.params.id)) {
+      ctx.body = await itemSchema.findById(ctx.params.id);
+    }
+    if (!ctx.body) {
+      ctx.response.status = 404;
+      ctx.body = { error: 'Not found' };
+    }
   });
 
   router.post('/', async (ctx) => {
@@ -19,16 +31,34 @@ export default router => {
     }
   });
 
-  // router.put('/', async (ctx) => {
-  //   const item = (ctx.request.body.title) ? ctx.request.body.title.trim() : '';
-  //   ctx.body = 'success bitch';
-  // });
+  router.put('/:id', async (ctx) => {
+    ctx.body = '';
+    ctx.request.body.title = (ctx.request.body.title) ? ctx.request.body.title.trim() : '';
+    if (ctx.request.body.title) {
+      if (mongoose.Types.ObjectId.isValid(ctx.params.id)) {
+        ctx.request.body.time = new Date();
+        ctx.body = await itemSchema.findByIdAndUpdate(ctx.params.id, ctx.request.body);
+      }
+      if (!ctx.body) {
+        ctx.response.status = 404;
+        ctx.body = { error: 'Not found' };
+      }
+    } else {
+      ctx.response.status = 400;
+      ctx.body = { error: 'Validation error' };
+    }
+  });
 
-  // router.delete('/:id', async (ctx) => {
-  //   ctx.body = ctx.params.id;
-  //   // ctx.body = await itemSchema.findByIdAndRemove(ctx.params.id);
-  //   // ctx.body = 'success bitch';
-  // });
+  router.delete('/:id', async (ctx) => {
+    ctx.body = '';
+    if (mongoose.Types.ObjectId.isValid(ctx.params.id)) {
+      ctx.body = await itemSchema.findByIdAndRemove(ctx.params.id);
+    }
+    if (!ctx.body) {
+      ctx.response.status = 404;
+      ctx.body = { error: 'Not found' };
+    }
+  });
 
   return router;
 
